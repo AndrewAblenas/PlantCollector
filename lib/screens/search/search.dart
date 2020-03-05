162987178f61d22d633365007f673770f0ffd949
Andrew Ablenas <a.ablenas@gmail.com>
2020-03-04 17:51:49 -0800

    Single update, search screed didn't show tab bar.

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -4,6 +4,7 @@ import 'package:plant_collector/models/app_data.dart';
 import 'package:plant_collector/models/builders_general.dart';
 import 'package:plant_collector/screens/search/widgets/search_bar.dart';
 import 'package:plant_collector/screens/template/screen_template.dart';
+import 'package:plant_collector/widgets/bottom_bar.dart';
 import 'package:provider/provider.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -12,6 +13,7 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenTemplate(
+      bottomBar: BottomBar(selectionNumber: 4),
       screenTitle: 'Search',
       child: Column(
         mainAxisSize: MainAxisSize.min,
